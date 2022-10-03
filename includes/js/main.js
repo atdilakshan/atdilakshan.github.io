a13a1dcef526f7e276ab76ccd85bee6d820634e0
atdilakshan <dilakshan0501@gmail.com>
2022-10-04 02:31:53 +0530

    all set

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,73 +1,36 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+// navbar toggle
+const navbarCollapse = document.querySelector(".fa-bars");
+const navbarLists = document.querySelector(".navbar-lists");
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+navbarCollapse.addEventListener("click", function () {
+  navbarLists.classList.toggle("active");
+  navbarCollapse.classList.toggle("active");
 });
+
+// skill list filter
+const skillCategotyList = document.querySelectorAll(".skill-categoty-list");
+const skillBox = document.querySelectorAll(".skill-box");
+
+for (let i = 0; i < skillCategotyList.length; i++) {
+  skillCategotyList[i].addEventListener("click", function () {
+    for (let j = 0; j < skillCategotyList.length; j++) {
+      skillCategotyList[j].classList.remove("skill-categoty-list-active");
+    }
+    this.classList.add("skill-categoty-list-active");
+
+    const dataFilter = this.getAttribute("data-filter");
+
+    for (let k = 0; k < skillBox.length; k++) {
+      skillBox[k].classList.remove("active");
+      skillBox[k].classList.add("hide");
+
+      if (
+        skillBox[k].getAttribute("data-item") == dataFilter ||
+        dataFilter == "all"
+      ) {
+        skillBox[k].classList.remove("hide");
+        skillBox[k].classList.add("active");
+      }
+    }
+  });
+}
